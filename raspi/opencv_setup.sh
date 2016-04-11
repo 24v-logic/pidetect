ff7fc035c6cec54fffc74e3c88630208f13b0dc2
Joe Prine <josephprine@gmail.com>
2016-04-11 18:21:32 -0400

    Reconfigured the setup shell scripts

--- raspi/opencv_setup.sh
+++ raspi/opencv_setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+echo "Installing numpy"
+pip install numpy
 
+echo "Installing OpenCV 3.0.0"
 cd ~/opencv-3.0.0/
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D INSTALL_C_EXAMPLES=ON \
-	-D INSTALL_PYTHON_EXAMPLES=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.0.0/modules \
-	-D BUILD_EXAMPLES=ON
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.0.0/modules -D BUILD_EXAMPLES=ON ..
+
+make -j4
+
+sudo make install 
+
+sudo ldconfig
+
--- raspi/pi_setup.sh
+++ raspi/pi_setup.sh
@@ -35,13 +35,6 @@ source ~/.profile
 echo "Setting up virtual environment (cv)"
 mkvirtualenv cv
 workon cv
-echo "Installing numpy on cv. . ."
-pip install numpy
 
-echo "Installing OpenCV 3.0.0"
-cd ~/opencv-3.0.0/
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.0.0/modules -D BUILD_EXAMPLES=ON ..
 
-echo "Please check output of CMake.  Thank you."
+
